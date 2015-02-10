ecd623cf75e01ecd1cadfc8968a7c0a99164c358
Aurélien Hervé <mail@aurelien-herve.com>
2015-02-10 15:34:47 +0100

    test helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
+ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+
 RSpec.configure do |config|
   config.include Devise::TestHelpers, type: :controller
 

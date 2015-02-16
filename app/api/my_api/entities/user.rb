115096327c9a9ce9bad6f732466caf753928bf85
Aurélien Hervé <mail@aurelien-herve.com>
2015-02-16 20:40:59 +0100

    un peu tout dans /me

--- app/api/my_api/entities/user.rb
+++ app/api/my_api/entities/user.rb
@@ -2,10 +2,12 @@ module MyApi
   module Entities
     class User < Grape::Entity
       expose :id 
+      expose :uid
       expose :provider
       expose :image
-
       expose :name
+      expose :allowed_moves_count
+      expose :last_charged
 
     end
   end

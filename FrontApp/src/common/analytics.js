36a4b2d20845163d2fabf53c6cc99a0a3aa9e09d
Alexandre Hervé <alexandre.herve9@gmail.com>
2015-02-16 18:48:30 +0100

    wip

--- FrontApp/src/common/analytics.js
+++ FrontApp/src/common/analytics.js
@@ -1,6 +1,12 @@
 do (app=angular.module "analytics", [
   'restangular'
 ]) ->
+  app.run [
+    'Restangular',
+    (Restangular)->
+      
+      
+  ]
   app.factory('Analytics', [
     'Restangular',
     (Restangular)->
